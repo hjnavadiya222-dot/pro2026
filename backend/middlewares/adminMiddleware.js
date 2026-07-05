bc1616ed2f79e15c1917c1247d9b91c724b1b4cf
harnilnavadiya <harnilnavadiya.it23d1@scet.ac.in>
2026-07-05 11:06:05 +0530

    fix: change User model import paths to lowercase to support case-sensitive OS environments

--- backend/middlewares/adminMiddleware.js
+++ backend/middlewares/adminMiddleware.js
@@ -1,4 +1,4 @@
-import { User } from "../models/User.js";
+import { User } from "../models/user.js";
 
 // Admin Middleware: Restrict Access to Admins Only
 export const adminMiddleware = async (req, res, next) => {
